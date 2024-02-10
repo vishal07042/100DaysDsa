83c20f77f1c07910cd4cf144d957baf56e62546b
vishal mehta <professionalprovishal@gmail.com>
2024-02-10 10:57:19 +0530

    10feb24 10:57

--- unit2.cpp
+++ unit2.cpp
@@ -4,54 +4,66 @@ using namespace std;
 
 int main()
 {
-    vector<int> p = {3, 3, 4, 5, 5, 6, 6, 9, 9};
-         //vector<int> p={1,1,2,2,4,5,5};
-        //  vector<int> p={1 ,1, 3, 5, 5 };
-    //   vector<int> p= {1, 1, 4, 4, 15};
+  //  vector<int> arr = {3, 3, 4, 4, 6, 7, 7, 9, 9};
+    // vector<int> arr={1,1,2,2,4,5,5};
+     // vector<int> arr={1 ,1, 3, 5, 5 };
+     vector<int> arr= {1, 1, 4, 4, 15};
 
-
-    int n = p.size();
-    vector <int > q; 
+    int n = arr.size();
+    vector<int> q;
 
     int s = 0;
     int e = n - 1;
     int mid = s + (e - s) / 2;
     int ans = -1;
 
+    while (s <= e)
+    {
 
+        if(arr[mid]!=arr[mid+1]  ){
+            if(arr[mid]!=arr[mid-1]){ 
+                q.push_back(arr[mid]);
+                break;
+             }
+            
+            
+         }
 
-         while(s<=e){ 
+        if (arr[s] + arr[e] == arr[s + 1] + arr[e - 1])
+        {
 
+        }
 
+        else
+        {
 
+            if (arr[s] != arr[s + 1])
+            {
 
+             
+                if (arr[s] != arr[s - 1])
+                {
+                    q.push_back(arr[s]);
+                }
+            }else if(arr[e]!=arr[e+1]){
 
-            if(p[s]+p[e] == p[s+1]+p[e-1]){
+                if(arr[e]!=arr[e-1]){
+                    q.push_back(arr[e]);
+                  }
                 
-                
-            }else{
-
-                if(p[s]==p[s+1] or p[s]==p[s-1]){ 
-                    q.push_back(p[e]);
-                      
-                 }else{
-                    q.push_back(p[s]);
-                 }
 
             }
-            s=s+2 ;
-                e=e-2;
-        
-            
-       }
 
+          
+        }
+        s = s + 2;
+        e = e - 2;
+    }
 
-       for(int x:q) {
-        cout << x;
-       }
-
-
-
+    for (int x : q)
+    {
+         cout << x;
+    }
 
     return 0;
 }
